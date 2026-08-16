240a1594f2150c9812997d89d15b2728dec82d82
Romulo <romin.nf@gmail.com>
2026-08-16 17:41:12 -0300

    tcplistener

--- main.go
+++ main.go
@@ -1,27 +1,78 @@
 package main
+
 import (
-	"log"
-	"io"
+	"bytes"
+	"net"
+	"errors"
 	"fmt"
-	"os"
+	"io"
+	"log"
 )
 
-func main(){
-	//args:= os.Args
-	path := "message.txt"
-	file,err:= os.Open(path)
-	//fmt.Println(args[1])
-	if err!= nil{
-		log.Fatal("error" ,"error",err)
-	}
-	for {
-		data := make([]byte,8)
-		bytesRead ,err:= file.Read(data)
-		if err== io.EOF{
-			return
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	lines := make(chan string)
+	go func() {
+		defer f.Close()
+		defer close(lines)
+		currentLineContents := ""
+		for {
+			data := make([]byte, 8)
+			n, err := f.Read(data)
+			if err != nil {
+				if currentLineContents != "" {
+					lines <- currentLineContents
+				}
+				if errors.Is(err, io.EOF) {
+					break
+				}
+				fmt.Printf("error: %s\n", err.Error())
+				return
+			}
+			data = data[:n]
+
+			if i := bytes.IndexByte(data, '\n'); i != -1 {
+				currentLineContents += string(data[:i])
+				data = data[i+1:]
+				lines <- currentLineContents
+				currentLineContents = ""
+			}
+			currentLineContents += string(data)
+
 		}
-		fmt.Printf("read: %s\n",string(data[:bytesRead]))
+		if len(currentLineContents) != 0 {
+			lines <- currentLineContents
+		}
+	}()
 
+	return lines
+}
+func main() {
+	port := ":42069"
+	ear,err := net.Listen("tcp",port)
+
+	if err != nil {
+		log.Fatal("error", "error", err)
 	}
+	defer ear.Close()
+	for {
+		connection , err := ear.Accept()
+		if err != nil {
+			log.Fatal("error", "error", err)
+		}
+		fmt.Println("Connection accepted")
 
+
+		/*
+		path := "message.txt"
+		file, err := os.Open(path)
+		if err != nil {
+			log.Fatal("error", "error", err)
+		}
+		*/
+		lines := getLinesChannel(connection)
+		for line := range lines {
+			fmt.Printf("%s\n",line)
+		}
+	fmt.Println("The connection is being closed ")
+}
 }
